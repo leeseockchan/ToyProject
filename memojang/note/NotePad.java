5972bd34c2dc070cf7a20c9b8a69e6a1e6dc79a8
leeseokchan <dltrjcks97@gmail.com>
2025-09-30 17:43:31 +0900

    ArrayList 적용 및 삭제 기능 추가

--- memojang/note/NotePad.java
+++ memojang/note/NotePad.java
@@ -1,81 +1,89 @@
 package memojang.note;
 
+import memojang.usecase.NoteService;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class NotePad {
 
-    private int noteLength = 0;
-    private final NoteEntity[] noteEntities;
-    private final int NOTE_SIZE = 20;
+    private final List<NoteEntity> noteEntities;
+    private final Scanner scanner;
+//    private int noteLength = 0;
+//    private final int NOTE_SIZE = 20;
 
-    public NotePad(){
-        this.noteEntities = new NoteEntity[NOTE_SIZE];
+    public NotePad(List<NoteEntity> list, Scanner scanner){
+        this.noteEntities = list;
+        this.scanner = scanner;
     }
 
 //    메모 전체 보기
     public void printAllNotes() {
-        System.out.println("");
-        if (0 == this.noteLength) {
-            System.out.println("작성된 메모가 없습니다");
-            System.out.println("");
+        System.out.println("-- 메모 전체 보기 --");
+        if (noteEntities.isEmpty()) {
+            System.out.println("작성된 메모가 없습니다. \n");
             return;
         }
-        for (int i = 0; i < this.noteLength; i++) {
-            NoteEntity noteEntity = noteEntities[i];
+        for (int i = 0; i < this.noteEntities.size(); i++) {
+            NoteEntity noteEntity = noteEntities.get(i);
 
             String headLine = String.format("번호:%d 제목:%s 작성날짜:%s", i, noteEntity.getTitle(),
                     noteEntity.getLastUpdatedDateTime());
             System.out.println(headLine);
         }
-        System.out.println("");
+        System.out.println("=============================\n");
     }
 
 //    메모 상세 보기
     public void printNote() {
-        System.out.println("");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("-- 메모 상세 보기 --");
         System.out.println("확인할 메모의 번호를 입력해주세요");
-        int selectedNumber = scanner.nextInt();
-        NoteEntity noteEntity = noteEntities[selectedNumber];
-        if (null == noteEntity) {
-            System.out.println("작성된 메모가 없습니다");
-            System.out.println("");
+        int selectedNumber = Integer.parseInt(scanner.nextLine());
+
+        if (selectedNumber < 0 || selectedNumber >= noteEntities.size()) {
+            System.out.println("오류 : 작성되지 않은 메모 입니다. \n");
             return;
         }
-        System.out.println("");
+        NoteEntity noteEntity = noteEntities.get(selectedNumber);
+
+        System.out.println("=============================");
         String headLine = String.format("번호:%d 제목:%s", selectedNumber, noteEntity.getTitle());
         System.out.println(headLine);
-        System.out.println(noteEntity.getLastUpdatedDateTime());
-        System.out.println(noteEntity.getContent());
-        System.out.println("");
+        System.out.println("작성일 : " + noteEntity.getLastUpdatedDateTime());
+        System.out.println("내용 : " + noteEntity.getContent());
+        System.out.println("=============================\n");
     }
 
 //    메모 추가
     public void addNote() {
-        System.out.println("");
-        if (NOTE_SIZE == this.noteLength) {
-            System.out.println("메모가 가득 찼습니다");
-            System.out.println("");
-            return;
-        }
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("-- 메모 신규 등록 --");
+//        TODO ArrayList로 변경 해서 무쓸모
+//        if (NOTE_SIZE == this.noteLength) {
+//            System.out.println("메모가 가득 찼습니다");
+//            System.out.println("");
+//            return;
+//        }
         System.out.println("제목을 작성해주세요");
         String title = scanner.nextLine();
         System.out.println("본문을 작성해주세요");
         String content = scanner.nextLine();
 
-        noteEntities[this.noteLength++] = NoteEntity.newInstance(title, content);
+        noteEntities.add(NoteEntity.newInstance(title, content));
 
-        System.out.println("메모가 성공적으로 작성되었습니다");
-        System.out.println("");
+        System.out.println("메모가 성공적으로 작성되었습니다. \n");
     }
 
 //    메모 수정
     public void updateNote(){
-        System.out.println("");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("-- 메모 수정 --");
         System.out.println("수정하실 메모의 번호를 입력해주세요");
         int selectedNumber = Integer.parseInt(scanner.nextLine());
+
+//        인덱스 유효성 검사
+        if (selectedNumber < 0 || selectedNumber >= noteEntities.size()) {
+            System.out.println("오류 : 작성되지 않은 메모 입니다. \n");
+            return;
+        }
 
         System.out.println("수정 할 제목을 작성해주세요");
         String title = scanner.nextLine();
@@ -83,16 +91,25 @@ public class NotePad {
         System.out.println("수정 할 본문을 작성해주세요");
         String content = scanner.nextLine();
 
-        NoteEntity noteEntity = noteEntities[selectedNumber];
-        if (null == noteEntity) {
-            System.out.println("존재하지 않는 메모입니다");
-            return;
-        }
+        NoteEntity noteEntity = noteEntities.get(selectedNumber);
         noteEntity.updateTitle(title);
         noteEntity.updateContent(content);
 
-        System.out.println("메모가 수정되었습니다");
-        System.out.println("");
+        System.out.println("메모가 수정되었습니다. \n");
+    }
+
+    public void removeNote(){
+        System.out.println("-- 메모 삭제 --");
+        System.out.println("삭제 할 메모장 번호를 입력해 주세요");
+        int selectedNumber = Integer.parseInt(scanner.nextLine());
+
+        if (selectedNumber < 0 || selectedNumber >= noteEntities.size()) {
+            System.out.println("오류 : 작성되지 않은 메모 입니다. \n");
+            return;
+        }
+
+        NoteEntity noteEntity = noteEntities.remove(selectedNumber);
+        System.out.println("메모가 삭제 되었습니다. \n");
     }
 
 }
--- memojang/usecase/NoteService.java
+++ memojang/usecase/NoteService.java
@@ -1,47 +1,60 @@
 package memojang.usecase;
 
+import memojang.note.NoteEntity;
 import memojang.note.NotePad;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NoteService {
     private final NotePad notePad;
+    private final Scanner scanner;
 
     public NoteService() {
-        this.notePad = new NotePad();
+        List<NoteEntity> notes = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+        this.notePad = new NotePad(notes, scanner);
     }
 
     public void run() {
 
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-
             System.out.println("안녕하세요. 메모장입니다");
             System.out.println("1.메모 전체보기 2.메모보기 3.메모작성 4.메모수정 5.메모삭제 6.종료");
             System.out.println("번호를 입력해주세요");
 
-            int selectedNumber = scanner.nextInt();
+            int selectedNumber;
+            try{
+                String input = scanner.nextLine();
+                selectedNumber = Integer.parseInt(input);
+            } catch(NumberFormatException e){
+                System.out.println("\n 오류 : 잘못된 입력 입니다. 숫자만 입력 해주세요 \n");
+                continue;
+            }
 
             if (1 == selectedNumber) {
-                // TODO : 메모 전체보기 처리
+// TODO : 메모 전체보기 처리
                 notePad.printAllNotes();
             } else if (2 == selectedNumber) {
-                // TODO : 메모 상세보기 처리
+// TODO : 메모 상세보기 처리
                 notePad.printNote();
             } else if (3 == selectedNumber) {
-                // TODO : 메모 작성하기 처리
+// TODO : 메모 작성하기 처리
                 notePad.addNote();
             } else if (4 == selectedNumber) {
-                // TODO : 메모 수정하기 처리
+// TODO : 메모 수정하기 처리
                 notePad.updateNote();
             } else if (5 == selectedNumber) {
-
+// TODO : 메모 삭제 하기 처리
+                notePad.removeNote();
             } else if (6 == selectedNumber) {
+// TODO : 메모 프로그램 종료
+                System.out.println("프로그램을 종료합니다. 안녕히 가세요");
+                this.scanner.close();
                 break;
             } else {
-                System.out.println("");
-                System.out.println("번호를 다시입력해주세요");
-                System.out.println("");
+                System.out.println("\n 오류: 1부터 6까지의 번호를 다시 입력해주세요.\n");
             }
         }
     }
